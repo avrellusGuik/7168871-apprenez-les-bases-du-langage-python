92ef3df2e9d33c38836d4f6cba60536f36da7eb2
avrellusGuik <Oyouaavrelle@gmail.com>
2024-07-23 22:45:20 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
 # Écrivez votre code ici !
+nom = "Oyoua"
+age = 20
+Taille = 1.70
+boleen = True
+print(f"je m'appelle {nom}, j'ai {age} ans, je messure {Taille}m")
+type(nom)
+type(age)
+type(boleen)
